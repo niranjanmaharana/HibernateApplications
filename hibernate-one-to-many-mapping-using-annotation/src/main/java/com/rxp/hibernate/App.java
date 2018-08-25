ebfb114e7ef1e99b28a96c2a31475911f03453ec
Niranjan Maharana <niranjanmaharana95@gmail.com>
2018-08-25 23:16:56 +0530

    hibernate-one-to-many-mapping-using-annotation

---
+++ hibernate-one-to-many-mapping-using-annotation/src/main/java/com/rxp/hibernate/App.java
@@ -0,0 +1,32 @@
+package com.rxp.hibernate;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+
+import com.rxp.hibernate.domain.Department;
+import com.rxp.hibernate.domain.Employee;
+import com.rxp.hibernate.util.HibernateUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Session session = HibernateUtil.getSession();
+		Department department = new Department("CSE");
+		Serializable deptNo = session.save(department);
+		session.beginTransaction().commit();
+		System.out.println("Department saved with deptNo : " + deptNo);
+		department.setDeptNo((Integer)deptNo);
+    	Employee employee = new Employee("Niranjan", 30L, department);
+    	Serializable empNo = session.save(employee);
+    	session.beginTransaction().commit();
+    	System.out.println("Employee saved with empNo : " + empNo);
+    	
+    	System.out.println("Saved Department : " + session.get(Department.class, deptNo));
+    }
+}
